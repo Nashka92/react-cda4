cdfce675be523ca160fafc3b31bb7ae1112dfc3e
Anna RAACH <annagraph92@gmail.com>
2024-05-13 14:53:32 +0200

    refacto code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import TabPersons from "./components/TabPersons";
 import "bootstrap/dist/css/bootstrap.min.css";
+import TabPersons from "./components/TabPersons";
 import Listing from "./components/Listing";
 
 function App() {
