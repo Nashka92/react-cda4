6c4a5230d5a2a66031a78dd955b096eefe1edb38
Anna RAACH <annagraph92@gmail.com>
2024-05-13 12:16:10 +0200

    remove useless comment

--- src/components/TabPersons.js
+++ src/components/TabPersons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Assurez-vous que Bootstrap est importé si nécessaire
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function TabPersons() {
     const personsTab = [
