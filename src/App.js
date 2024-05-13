e683b41f0d45fccbd42be2fb45698f865e46fc6f
Anna RAACH <annagraph92@gmail.com>
2024-05-13 14:52:57 +0200

    remove useless component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import TabPersons from "./components/TabPersons";
-import Title from "./components/Title";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Listing from "./components/Listing";
 
 function App() {
   return (
     <div className="App">
-      <Title />
       <TabPersons />
       <Listing />
     </div>
