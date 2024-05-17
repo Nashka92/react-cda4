80e17b2092dc33092095940ccfc4c1ab30292419
Anna RAACH <annagraph92@gmail.com>
2024-05-17 16:21:40 +0200

    add toast

--- src/components/exercice_10/utils/useFavorites.js
+++ src/components/exercice_10/utils/useFavorites.js
@@ -1,4 +1,6 @@
 import React, { createContext, useState, useContext } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const FavoritesContext = createContext();
 
@@ -8,18 +10,24 @@ export const FavoritesProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
 
   const addCountryToFav = (country) => {
-    const isAlreadyFav = favorites.some(fav => fav.cca3 === country.cca3);
+    const isAlreadyFav = favorites.some((fav) => fav.cca3 === country.cca3);
     if (!isAlreadyFav) {
       setFavorites([...favorites, country]);
+      toast.success(`${country.name.common} a été ajouté aux favoris!`);
+    } else {
+      toast.info(`${country.name.common} est déjà dans les favoris.`);
     }
   };
 
   const removeCountryToFav = (country) => {
-    setFavorites(favorites.filter(fav => fav.cca3 !== country.cca3));
+    setFavorites(favorites.filter((fav) => fav.cca3 !== country.cca3));
   };
 
   return (
-    <FavoritesContext.Provider value={{ favorites, addCountryToFav, removeCountryToFav }}>
+    <FavoritesContext.Provider
+      value={{ favorites, addCountryToFav, removeCountryToFav }}
+    >
+      <ToastContainer />
       {children}
     </FavoritesContext.Provider>
   );
